059d17aae6730b7d745fd0d680035e62a9ebc7da
Vlad Makhnenko <vlmakh@gmail.com>
2023-11-07 21:11:49 +0200

    create About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "@/app/page.module.css";
+
+export default function About() {
+  return (
+    <div className={styles.center}>
+      <h1>About Page</h1>
+    </div>
+  );
+}
