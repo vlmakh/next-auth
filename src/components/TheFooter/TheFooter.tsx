43e88be3d4bb048c8314547d9f87029c0d8e93b4
Vlad Makhnenko <vlmakh@gmail.com>
2023-11-07 21:02:51 +0200

    add header-footer

---
+++ src/components/TheFooter/TheFooter.tsx
@@ -0,0 +1,30 @@
+import styles from "@/app/page.module.css";
+import Image from "next/image";
+
+export const TheFooter = () => {
+  return (
+    <div className={styles.description}>
+      <p>
+        Get started by editing&nbsp;
+        <code className={styles.code}>src/app/page.tsx</code>
+      </p>
+      <div>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          By{" "}
+          <Image
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            className={styles.vercelLogo}
+            width={100}
+            height={24}
+            priority
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
