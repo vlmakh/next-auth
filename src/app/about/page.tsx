5886b1046099cb49d9b3a49a898e49dc41b67648
Vlad Makhnenko <vlmakh@gmail.com>
2023-11-09 06:17:53 +0200

    add metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 import styles from "@/app/page.module.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About | Next Auth",
+};
 
 export default function About() {
   return (
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,16 +1,23 @@
 import styles from "@/app/page.module.css";
+import { Metadata } from "next";
 
 type Props = {
-    params: {
-        id: string
-    }
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({
+  params: { id },
+}: Props): Promise<Metadata> {
+  return { title: id };
 }
 
-export default function ProductPage({params: {id}}: Props) {
+export default function ProductPage({ params: { id } }: Props) {
   return (
     <div className={styles.center}>
-          <h1>Product Page</h1>
-          <h1> {id}</h1>
+      <h1>Product Page</h1>
+      <h1> {id}</h1>
     </div>
   );
-};
+}
