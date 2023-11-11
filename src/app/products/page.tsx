692750c45d146aedb9459a882cb79ae4ce225f15
Vlad Makhnenko <vlmakh@gmail.com>
2023-11-11 19:50:40 +0200

    add data fetch

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,29 @@
 import styles from "@/app/page.module.css";
+import { Metadata } from "next";
+import Link from "next/link";
 
-export default function Products() {
+export const metadata: Metadata = {
+  title: "Products | Next Auth",
+};
+
+async function getData() {
+  const res = await fetch('https://fakestoreapi.com/products')
+
+  return res.json()
+}
+
+export default async function Products() {
+  const products = await getData()
+
+  // console.log(products)
+
   return (
-    <div className={styles.center}>
+    <>
       <h1>Products Page</h1>
-    </div>
+
+      <ul>
+        {products.map((product: any) => (<li key={product.id}><Link href={`/products/${product.id}`}>{ product.title}</Link></li>))}
+      </ul>
+    </>
   );
 };
